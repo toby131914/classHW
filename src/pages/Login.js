910bbf06b8a03d359e22400875c3055e617f71ca
chiangyiyang <chiangyiyang@gmail.com>
2021-11-23 17:34:08 +0800

    加入link

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import './Login.css';
 
 function Login() {
   return (
     <div className="Login">
       Login
-      <button className="btnGoogle">Google</button>
+      <button className="btnGoogle">
+      <Link to="/profile" className="login-link">
+      Google
+      </Link>
+      </button>
       <button className="btnLine">LINE</button>
       <button className="btnFB">Facebook</button>
     </div>
