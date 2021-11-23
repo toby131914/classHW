61c12903954402d8bf9edb6b1194d2be99af7561
chiangyiyang <chiangyiyang@gmail.com>
2021-11-23 17:32:16 +0800

    修改class

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -4,7 +4,7 @@ import './Splash.css';
 function Splash() {
   return (
     <div className="Splash">
-      <Link to="/login" className="link">
+      <Link to="/login" className="title-link">
         災害回報系統
       </Link>
       <span>DIRS <span className="ver">v 1.1.2</span></span>
