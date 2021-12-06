0d8d33fb6c1c8fe1b1b4f129029f23f7e23ce4a5
chiangyiyang <chiangyiyang@gmail.com>
2021-12-06 23:56:44 +0800

    更新PostListItem畫面

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -2,27 +2,27 @@ import './PostListItem.css';
 
 function PostListItem({ id, title, timestamp, img, content }) {
   return (
-    <div className="PostListItem">
+    <>
       {title ?
-        <>
-          <div className="row">
+        <div>
+          <div className="PostListItem flex-row">
             <img src={img} className="item-img" alt={title} />
-            <div className="col">
-              <div className="item-header row">
+            <div className="flex-col">
+              <div className="item-header flex-row">
                 <div>{title}</div>
                 <div>{timestamp}</div>
               </div>
               <div className="item-content">{content}</div>
             </div>
           </div>
-              <hr />
-        </>
+          <hr />
+        </div>
         :
         <>
           Loading...
         </>
       }
-    </div>
+    </>
   );
 }
 
