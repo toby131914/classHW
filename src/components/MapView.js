0ff296a67a7c816fc4ee504d767c6c29324cda16
chiangyiyang <chiangyiyang@gmail.com>
2021-12-08 12:11:04 +0800

    使用Cesium

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,18 +1,45 @@
-import { useState } from 'react';
-import PostList from "./PostList";
+import {
+  Viewer,
+  Math,
+  Cartesian3,
+} from "cesium";
+import { useEffect } from "react";
 import './MapView.css';
-import map from "../assets/img/map.png";
-
 
 function MapView() {
 
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    const newViewer = new Viewer('cesiumContainer', {
+      shouldAnimate: true,
+      timeline: false,
+      animation: false,
+      shadow: false,
+      infoBox: false,
+      selectionIndicator: false,
+      geocoder: false,
+      homeButton: false,
+      baseLayerPicker: true,
+      navigationHelpButton: false,
+      sceneModePicker: false,
+    });
+    newViewer._cesiumWidget._creditContainer.style.display = 'none';
+
+    newViewer.camera.flyTo({
+      duration: 0.1,
+      destination: Cartesian3.fromDegrees(119, 21, 800000),
+      orientation: {
+        heading: Math.toRadians(0.0),
+        pitch: Math.toRadians(-60.0),
+        roll: 0.0
+      },
+    });
+  }, []);
+
+
   return (
     <div className="MapView flex-col">
-      <img
-        className="MapViewImg"
-        src={map}
-        alt="..."
-      />
+      <div id='cesiumContainer'></div>
     </div>
   );
 }
