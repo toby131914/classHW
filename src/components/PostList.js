c4d596f6a890a6b4e9f93c780a40c7ad48949616
toby131914 <t131914t@gmail.com>
2021-12-15 15:09:03 +0800

    00

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,9 +9,9 @@ function PostList() {
     setTimeout(() => {
       setPosts(
         [
-          { id: 1, title: "AAA", timestamp: "2021-11-26 15:30:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
-          { id: 2, title: "BBB", timestamp: "2021-11-26 15:31:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
-          { id: 3, title: "CCC", timestamp: "2021-11-26 15:32:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
+          { id: 1, title: "阿里山藝妓咖啡", timestamp: "2021-11-26 15:30:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
+          { id: 2, title: "卓武山咖啡", timestamp: "2021-11-26 15:31:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
+          { id: 3, title: "嵩岳咖啡", timestamp: "2021-11-26 15:32:30", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXBlVQ8gqAXGIoQHLUz0xWqX-IlVuANvFVQg&usqp=CAU", content: "He'll want to use your yacht, and I don't want this thing smelling like fish." },
         ]
       );
     }, 3000);
